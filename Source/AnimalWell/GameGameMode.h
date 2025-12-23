fe5dc231a0f6f7f495cd1d41a79143e75edbb72b
PersoAnima <persoanima.yan@gmail.com>
2025-12-24 03:14:38 +0800

    Add files via upload

---
+++ Source/AnimalWell/GameGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameGameMode.generated.h"
+
+UCLASS()
+class ANIMALWELL_API AGameGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+public:
+    AGameGameMode();
+};
